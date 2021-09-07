9a3c442fe91f497931ffcdc975f343247ab0d581
Scott Walter <scott.walter@nice.com>
2021-09-06 20:10:01 -0500

    WIP

--- e2e/test1.test.ts
+++ e2e/test1.test.ts
@@ -1,4 +1,6 @@
 import { test } from './baseFixtures';
+const v8toIstanbul = require('v8-to-istanbul');
+const fs = require('fs')
 
 function delay(time) {
   return new Promise(function(resolve) {
@@ -7,7 +9,37 @@ function delay(time) {
 }
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:4200")
+  await Promise.all([
+    page.coverage.startJSCoverage(),
+    page.coverage.startCSSCoverage()
+  ]);
+
+    // await page.goto("http://localhost:4200")
+    await page.goto("http://localhost:8080")
+  })
+
+  test.afterEach(async ({ page }) => {
+    const [jsCoverage, cssCoverage] = await Promise.all([
+      page.coverage.stopJSCoverage(),
+      page.coverage.stopCSSCoverage(),
+    ]);
+
+    console.log(jsCoverage);
+
+    // const coverage = await page.coverage.stopJSCoverage();
+    for (const entry of jsCoverage) {
+
+    //   console.log('KEYS', Object.keys(entry));
+    //   console.log('URL', entry.url);
+    //   // console.log(entry.source);
+      const converter = new v8toIstanbul('', 0, { source: entry.source });
+      await converter.load();
+      converter.applyCoverage(entry.functions);
+      console.log(JSON.stringify(converter.toIstanbul()));
+
+      const fd = fs.openSync('scott.json', 'a')
+      fs.writeSync(fd, JSON.stringify(converter.toIstanbul()));
+    }
   })
 
 test('click button', async ({ page }) => {
